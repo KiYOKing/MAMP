45cfec325594c65ffc49a023fc0f926a19976a66
KiYO <kiyoto.raputa@gmail.com>
2022-01-05 09:13:47 +0900

    メモアプリ追加

---
+++ memo/index.php
@@ -0,0 +1,51 @@
+<?php
+//$db = new mysqli('localhost:8889','root','root','mydb');
+require('dbconnect.php');
+
+/*最大ページ数を求める*/
+$counts = $db->query('select count(*) as cnt from memos');
+$count = $counts->fetch_assoc();
+$max_page = floor(($count['cnt']+1)/5+1);
+
+$stmt = $db->prepare('select * from memos order by id desc limit ?, 5');
+if (!$stmt){
+    die($db->error);
+}
+$page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_NUMBER_INT);
+$page = ($page ?: 1);
+$start = ($page -1) * 5;
+$stmt->bind_param('i', $start);
+$result = $stmt->execute();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>メモ帳</title>
+</head>
+<body>
+    <h1>メモ帳</h1>
+    <p>👉<a href="input.html">新しいメモ</a></p>
+
+    <?php $stmt->bind_result($id, $memo, $created); ?>
+    <?php while ($stmt->fetch()): ?>
+        <div>
+            <h2><a href="memo.php?id=<?php echo $id; ?>"><?php echo htmlspecialchars(mb_substr($memo,0,50)); ?></a></h2>
+            <time><?php echo htmlspecialchars($created); ?></time>
+        </div>
+        <hr>
+    <?php endwhile; ?>
+    
+    <p>
+        <?php if ($page>1): ?>
+            <a href="?page=<?php echo $page-1; ?>"><?php echo $page-1; ?>ページ目へ</a>
+        <?php endif; ?>
+        <?php if ($page<$max_page): ?>
+            <a href="?page=<?php echo $page+1; ?>"><?php echo $page+1; ?>ページ目へ</a>
+        <?php endif; ?>
+        
+    </p>
+</body>
+</html>
---
+++ memo/input_do.php
@@ -0,0 +1,21 @@
+<?php
+$memo = filter_input(INPUT_POST, 'memo', FILTER_SANITIZE_SPECIAL_CHARS);
+//$db = new mysqli('localhost:8889','root','root','mydb');
+require('dbconnect.php');
+
+$stmt = $db->prepare('insert into memos (memo) values(?)');
+
+if (!$stmt):
+    die($db->error);
+endif;
+
+$stmt->bind_param('s', $memo);
+$ret = $stmt->execute();
+
+if($ret):
+    echo '登録されました';
+    echo '<br>👉<a href="index.php">トップへ戻る</a>';
+else:
+    echo $db->error;
+endif;
+?>
